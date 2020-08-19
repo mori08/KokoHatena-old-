6e7b5ee27cadd426b55bc23be67de7ad6a46fac1
森本 <mrmt0814@gmail.com>
2020-08-20 00:53:09 +0900

    Add: MyLibraryの追加

--- KokoHatena/Src/Main.cpp
+++ KokoHatena/Src/Main.cpp
@@ -1,5 +1,5 @@
 ﻿
-#include<Siv3D.hpp>
+#include"MyLibrary/MyLibrary.hpp"
 
 namespace
 {
@@ -11,8 +11,16 @@ void Main()
 {
 	Window::Resize(WINDOW_SIZE);
 
+	// アセット管理
+	Kokoha::registerAsset(U"asset/");
+	FontAsset::Register(U"10", 10, Typeface::Medium);
+	FontAsset::Register(U"15", 15, Typeface::Medium);
+	FontAsset::Register(U"20", 20, Typeface::Medium);
+	FontAsset::Register(U"30", 30, Typeface::Medium);
+	FontAsset::Register(U"40", 40, Typeface::Medium);
+
 	while (System::Update())
 	{
-		
+		Kokoha::showFPS();
 	}
 }
