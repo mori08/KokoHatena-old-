e52bce25508f46d40e962370c33a56e5e3b2e308
森本 <mrmt0814@gmail.com>
2020-08-20 14:34:36 +0900

    Add: MainにSceneManagerを追加

--- KokoHatena/Src/Main.cpp
+++ KokoHatena/Src/Main.cpp
@@ -1,6 +1,9 @@
 ﻿
 #include"MyLibrary/MyLibrary.hpp"
 
+// シーン
+#include "Scene/Title/TitleScene.hpp"
+
 namespace
 {
 	// 画面サイズ
@@ -9,7 +12,13 @@ namespace
 
 void Main()
 {
+	// ウィンドウの設定
 	Window::Resize(WINDOW_SIZE);
+	Window::SetTitle(U"55?");
+
+	// 描画設定
+	Scene::SetBackground(Kokoha::MyBlack);
+	Scene::Resize(WINDOW_SIZE);
 
 	// アセット管理
 	Kokoha::registerAsset(U"asset/");
@@ -19,8 +28,14 @@ void Main()
 	FontAsset::Register(U"30", 30, Typeface::Medium);
 	FontAsset::Register(U"40", 40, Typeface::Medium);
 
+	// シーン
+	MyApp sceneManager;
+	sceneManager.add<Kokoha::TitleScene>(SceneName::TITLE);
+
 	while (System::Update())
 	{
+		sceneManager.update();
+
 		Kokoha::showFPS();
 	}
 }
